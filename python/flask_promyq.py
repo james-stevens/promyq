d1f229d62db76459785782e2ee216347e79c0c8a
James Stevens <github@jrcs.net>
2024-09-17 15:30:04 +0000

    week & all time charts

--- python/flask_promyq.py
+++ python/flask_promyq.py
@@ -3,22 +3,35 @@
 # Alternative license arrangements possible, contact me for more information
 """ module to run the rest/api for user's site web/ui """
 
+import os
 import flask
 
 import promyq
 
 application = flask.Flask("PromYQ")
 
+with open("/etc/version") as fd:
+    this_version = fd.readline().strip()
+
 
 @application.route('/version', methods=['GET'])
 def get_version():
-    with open("/etc/version") as fd:
-        return flask.make_response(fd.readline(), 200)
-    return flask.make_response("ERROR: Failed to open version file", 503)
+    return flask.make_response(this_version, 200)
 
 
 @application.route('/promyq/config', methods=['GET'])
 def get_config():
+    my_config = {
+        "refresh_interval":
+        os.environ["PROMYQ_REFRESH_INTERVAL"] if "PROMYQ_REFRESH_INTERVAL" in os.environ else "300s",
+        "retention_time": os.environ["PROMYQ_RETENTION_TIME"] if "PROMYQ_RETENTION_TIME" in os.environ else "180d",
+        "version": this_version
+    }
+    return flask.make_response(flask.jsonify(my_config), 200)
+
+
+@application.route('/promyq/trades', methods=['GET'])
+def get_trades():
     try:
         my_promyq = promyq.PromYQ()
         return flask.make_response(my_promyq.current_config(), 200)
